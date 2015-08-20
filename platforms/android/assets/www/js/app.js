2f3d74ddd78183f73b8e3e3eeb4305dfbc7019ce
gopz4u <gopu.pk@gmail.com>
2015-08-20 18:37:30 +0800

    hi
    hi

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+                var options = new ContactFindOptions();
+                options.filter="";
+                options.multiple=true; 
+                var fields = ["*"];
+                navigator.contacts.find(fields, onSuccess, onError, options);
+            }
+
+function onSuccess(contacts) {
+                for (var i = 0; i < contacts.length; i++) {
+                    $("#contactList").append("<li>             <h1>"+contacts[i].phoneNumber[0].value+"</h1></li>");
+                    $("#contactList").listview("refresh");
+                }
+            }
+
+function onError()
+{
+alert("Some Error Occured");
